2052cf4c6406cf472d4be83472eb7b9a1666a64b
Kayywu <katherine.wosik@gmail.com>
2023-06-06 08:39:12 +0200

    src

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,27 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+    const link = document.createElement('a');
+    link.classList.add("gallery__link");
+    link.setAttribute("href", item.original);
+
+    const image = document.createElement('img');
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.setAttribute("alt", item.description)
+
+    link.append(image);
+    gallery.append(link);
+});
+
+var lightbox = new simpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
+
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+'use sctrict';
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+
+    if (data.duration === data.seconds) {
+        data.seconds = 0;
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+    }
+};
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.then(function (seconds){})
+.catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+            default:
+                break;
+    }
+})
